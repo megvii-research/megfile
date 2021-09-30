78ae4349ed9d710f4f1f5ec33e7099ebe652f985
Theo Li <liyang@megvii.com>
2021-09-30 18:52:17 +0800

    fix cli cat command (#58)

--- megfile/cli.py
+++ megfile/cli.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import sys
 import time
 
 import click
@@ -143,9 +145,8 @@ def touch(path: str):
 @cli.command(short_help='Concatenate any files and send them to stdout.')
 @click.argument('path')
 def cat(path: str):
-    with smart_open(path, 'r') as f:
-        for line in f:
-            click.echo(line)
+    with smart_open(path, 'rb') as file:
+        shutil.copyfileobj(file, sys.stdout.buffer)
 
 
 @cli.command(
