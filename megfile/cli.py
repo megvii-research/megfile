c4d6c22f2637bcb9fbecfcd6795be1c13d7859d1
phy <penghongyang@megvii.com>
2021-11-16 15:30:42 +0800

    修复cli cp 和 mv 行为

--- megfile/cli.py
+++ megfile/cli.py
@@ -6,7 +6,7 @@ import time
 import click
 
 from megfile.interfaces import FileEntry
-from megfile.smart import smart_copy, smart_getmd5, smart_getmtime, smart_getsize, smart_isfile, smart_makedirs, smart_move, smart_open, smart_remove, smart_rename, smart_scan_stat, smart_scandir, smart_stat, smart_sync, smart_touch, smart_unlink
+from megfile.smart import smart_copy, smart_getmd5, smart_getmtime, smart_getsize, smart_isdir, smart_isfile, smart_makedirs, smart_move, smart_open, smart_remove, smart_rename, smart_scan_stat, smart_scandir, smart_stat, smart_sync, smart_touch, smart_unlink
 from megfile.utils import get_human_size
 from megfile.version import VERSION
 
@@ -88,9 +88,16 @@ def ls(path: str, long: bool, recursive: bool, human_readable: bool):
     help=
     'Command is performed on all files or objects under the specified directory or prefix.'
 )
-def cp(src_path: str, dst_path: str, recursive: bool):
+@click.option(
+    '-T',
+    '--no-target-directory',
+    is_flag=True,
+    help='treat dst_path as a normal file.')
+def cp(
+        src_path: str, dst_path: str, recursive: bool,
+        no_target_directory: bool):
     copy_func = smart_sync if recursive else smart_copy
-    copy_func(src_path, dst_path)
+    copy_func(src_path, dst_path, no_target_directory=no_target_directory)
 
 
 @cli.command(short_help='Move files from source to dest.')
@@ -103,9 +110,18 @@ def cp(src_path: str, dst_path: str, recursive: bool):
     help=
     'Command is performed on all files or objects under the specified directory or prefix.'
 )
-def mv(src_path: str, dst_path: str, recursive: bool):
-    move_func = smart_move if recursive else smart_rename
-    move_func(src_path, dst_path)
+@click.option(
+    '-T',
+    '--no-target-directory',
+    is_flag=True,
+    help='treat dst_path as a normal file.')
+def mv(
+        src_path: str, dst_path: str, recursive: bool,
+        no_target_directory: bool):
+    if recursive or (smart_isdir(dst_path) and not no_target_directory):
+        smart_move(src_path, dst_path, no_target_directory=no_target_directory)
+    else:
+        smart_rename(src_path, dst_path)
 
 
 @cli.command(short_help='Remove files from path.')
