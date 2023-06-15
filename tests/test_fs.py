389f7493c987611b8269457e8c7ad0ac6404cc78
penghongyang <penghongyang@megvii.com>
2023-06-15 13:34:30 +0800

    fix(fs_glob): fix glob order

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -899,6 +899,17 @@ def test_fs_glob_returns_lexicographical_result(create_glob_fake_dirtree):
         list(fs.fs_glob('A/a/**.notExists', missing_ok=False))
 
 
+def test_fs_glob_ascending_alphabetical_order(filesystem):
+    files = []
+    for i in range(0, 4001, 500):
+        files.append(f'range_{str(i).zfill(4)}_{str(i+500).zfill(4)}.meta.json')
+    for filename in files:
+        with open(filename, 'w') as f:
+            pass
+    assert fs.fs_glob('*.json') == files
+    assert list(fs.fs_iglob('*.json')) == files
+
+
 def test_fs_glob_with_symbol_link(create_glob_fake_dirtree):
     # treat symbol link as file
     res = list(fs.fs_glob('A/b/*'))
