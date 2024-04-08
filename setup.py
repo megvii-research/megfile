4aac08b8baedae34d08d838110b6007b991020c4
Hongyang Peng <penghongyang@megvii.com>
2024-04-08 11:00:59 +0800

    perf: optimize source code installation (#350)

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-from setuptools import find_packages, setup
-from sphinx.setup_command import BuildDoc
 from importlib.machinery import SourceFileLoader
+
+from setuptools import find_packages, setup
+
+try:
+    from sphinx.setup_command import BuildDoc
+
+    cmdclass = {
+        "build_sphinx": BuildDoc,
+    }
+except ImportError:
+    cmdclass = {}
 
 
 def load_version(filename):
@@ -21,38 +30,36 @@ requirements = load_requirements("requirements.txt")
 test_requirements = load_requirements("requirements-dev.txt")
 
 setup(
-    name='megfile',
-    description='Megvii file operation library',
-    long_description=load_text('README.md'),
-    long_description_content_type='text/markdown',
-    version=load_version('megfile/version.py'),
-    author='megvii',
-    author_email='megfile@megvii.com',
-    url='https://github.com/megvii-research/megfile',
-    packages=find_packages(exclude=('tests', 'tests*', 'remof')),
-    entry_points={'console_scripts': ['megfile = megfile.cli:safe_cli']},
+    name="megfile",
+    description="Megvii file operation library",
+    long_description=load_text("README.md"),
+    long_description_content_type="text/markdown",
+    version=load_version("megfile/version.py"),
+    author="megvii",
+    author_email="megfile@megvii.com",
+    url="https://github.com/megvii-research/megfile",
+    packages=find_packages(exclude=("tests", "tests*", "remof")),
+    entry_points={"console_scripts": ["megfile = megfile.cli:safe_cli"]},
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
-    cmdclass={
-        'build_sphinx': BuildDoc,
-    },
+    cmdclass=cmdclass,
     tests_require=test_requirements,
     install_requires=requirements,
     extras_require={
-        "hdfs": load_requirements('requirements-hdfs.txt'),
+        "hdfs": load_requirements("requirements-hdfs.txt"),
     },
-    python_requires='>=3.8',
+    python_requires=">=3.8",
 )
