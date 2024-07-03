4e5d6fd112a06e269cfd47ca2d8017bedf49c358
Hongyang Peng <penghongyang@megvii.com>
2024-07-03 10:54:00 +0800

    perf:optimize default config values (#377)

--- docs/configuration/common.md
+++ docs/configuration/common.md
@@ -1,8 +1,10 @@
 ### Environment configurations
-- `MEGFILE_BLOCK_SIZE`: block size in some `open` func, like `http_open`, `s3_open`, default is `8MB`
-- `MEGFILE_MAX_BLOCK_SIZE`: max block size in some `open` func, like `http_open`, `s3_open`, default is `block size * 16`
-- `MEGFILE_MAX_BUFFER_SIZE`: max buffer size in some `open` func, like `http_open`, `s3_open`, default is `block size * 16`
+- `MEGFILE_BLOCK_SIZE`: default block size of read and write operate, unit is bytes, default is `8MB`
+- `MEGFILE_MAX_BLOCK_SIZE`: max write block size, unit is bytes, default is `128MB`
+- `MEGFILE_MAX_BUFFER_SIZE`: max read buffer size, unit is bytes, default is `128MB`
 - `MEGFILE_MAX_WORKERS`: max threads will be used, default is `32`
-- `MEGFILE_BLOCK_CAPACITY`: default cache capacity of block and concurrency, default is `16`
+- `MEGFILE_BLOCK_CAPACITY`: 
+    - default cache capacity of block, default is `16`
+    - if `MEGFILE_MAX_BUFFER_SIZE` and `MEGFILE_BLOCK_CAPACITY` are both set, `MEGFILE_BLOCK_CAPACITY` will be ignored
 - `MEGFILE_S3_CLIENT_CACHE_MODE`: s3 client cache mode, `thread_local` or `process_local`, default is `thread_local`, **it's a experimental feature.**
 - `MEGFILE_MAX_RETRY_TIMES`: default max retry times when catch error which may fix by retry, default is `10`
--- megfile/config.py
+++ megfile/config.py
@@ -1,12 +1,40 @@
 import os
+from logging import getLogger
 
-DEFAULT_BLOCK_SIZE = int(os.getenv('MEGFILE_BLOCK_SIZE') or 8 * 2**20)  # 8MB
-DEFAULT_MAX_BLOCK_SIZE = int(
-    os.getenv('MEGFILE_MAX_BLOCK_SIZE') or DEFAULT_BLOCK_SIZE * 16)  # 128MB
-DEFAULT_MAX_BUFFER_SIZE = int(
-    os.getenv('MEGFILE_MAX_BUFFER_SIZE') or DEFAULT_BLOCK_SIZE * 16)  # 128MB
+_logger = getLogger(__name__)
+
+DEFAULT_BLOCK_SIZE = int(os.getenv('MEGFILE_BLOCK_SIZE') or 8 * 2**20)
+
+if os.getenv('MEGFILE_MAX_BUFFER_SIZE'):
+    DEFAULT_MAX_BUFFER_SIZE = int(os.environ['MEGFILE_MAX_BUFFER_SIZE'])
+    if DEFAULT_MAX_BUFFER_SIZE < DEFAULT_BLOCK_SIZE:
+        DEFAULT_MAX_BUFFER_SIZE = DEFAULT_BLOCK_SIZE
+        _logger.warning(
+            "Env 'MEGFILE_MAX_BUFFER_SIZE' is smaller than block size, will not use buffer."
+        )
+    DEFAULT_BLOCK_CAPACITY = DEFAULT_MAX_BUFFER_SIZE // DEFAULT_BLOCK_SIZE
+    if os.getenv('MEGFILE_BLOCK_CAPACITY'):
+        _logger.warning(
+            "Env 'MEGFILE_MAX_BUFFER_SIZE' and 'MEGFILE_BLOCK_CAPACITY' are both set, 'MEGFILE_BLOCK_CAPACITY' will be ignored."
+        )
+elif os.getenv('MEGFILE_BLOCK_CAPACITY'):
+    DEFAULT_BLOCK_CAPACITY = int(os.environ['MEGFILE_BLOCK_CAPACITY'])
+    DEFAULT_MAX_BUFFER_SIZE = DEFAULT_BLOCK_SIZE * DEFAULT_BLOCK_CAPACITY
+else:
+    DEFAULT_MAX_BUFFER_SIZE = 128 * 2**20
+    DEFAULT_BLOCK_CAPACITY = 16
+
+if os.getenv('MEGFILE_MAX_BLOCK_SIZE'):
+    DEFAULT_MAX_BLOCK_SIZE = int(os.environ['MEGFILE_MAX_BLOCK_SIZE'])
+    if DEFAULT_MAX_BLOCK_SIZE < DEFAULT_BLOCK_SIZE:
+        DEFAULT_MAX_BLOCK_SIZE = DEFAULT_BLOCK_SIZE
+        _logger.warning(
+            "Env 'MEGFILE_MAX_BLOCK_SIZE' is smaller than block size, will be ignored."
+        )
+else:
+    DEFAULT_MAX_BLOCK_SIZE = max(128 * 2**20, DEFAULT_BLOCK_SIZE)
+
 GLOBAL_MAX_WORKERS = int(os.getenv('MEGFILE_MAX_WORKERS') or 32)
-DEFAULT_BLOCK_CAPACITY = int(os.getenv('MEGFILE_BLOCK_CAPACITY') or 16)
 DEFAULT_MAX_RETRY_TIMES = int(os.getenv('MEGFILE_MAX_RETRY_TIMES') or 10)
 
 # for logging the size of file had read or wrote
