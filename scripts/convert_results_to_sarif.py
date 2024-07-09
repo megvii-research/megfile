5da01bc0935fd8fdc4f2ec57d5383c87deb9a42d
penghongyang <penghongyang@megvii.com>
2024-07-09 13:23:02 +0800

    test

--- scripts/convert_results_to_sarif.py
+++ scripts/convert_results_to_sarif.py
@@ -120,11 +120,5 @@ if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s [%(levelname)s] %(message)s", level=logging.DEBUG)
 
-    data = sys.stdin
-    raise ValueError(data)
-    if data:
-        data = json.load(data)
-    else:
-        data = []
-    sarif = _to_sarif(data)
+    sarif = _to_sarif(json.load(sys.stdin.read()))
     json.dump(sarif, sys.stdout, indent=4)
