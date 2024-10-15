7445b2ea248bdbbb67b6fbb43252385d3b4634d2
penghongyang <penghongyang@megvii.com>
2024-10-15 16:43:07 +0800

    fix:fix static check

--- megfile/interfaces.py
+++ megfile/interfaces.py
@@ -189,7 +189,7 @@ class Readable(FileLike[AnyStr], ABC):
     def write(self, data: AnyStr) -> int:
         raise OSError("not writable")
 
-    def writelines(self, lines: Iterable[AnyStr]) -> None:  # pyre-ignore[14]
+    def writelines(self, lines: Iterable[AnyStr]) -> None:  # type: ignore
         raise OSError("not writable")
 
 
@@ -205,7 +205,7 @@ class Writable(FileLike[AnyStr], ABC):
         Return the number of bytes or string written.
         """
 
-    def writelines(self, lines: Iterable[AnyStr]) -> None:  # pyre-ignore[14]
+    def writelines(self, lines: Iterable[AnyStr]) -> None:  # type: ignore
         """Write `lines` to the file.
 
         Note that newlines are not added.
