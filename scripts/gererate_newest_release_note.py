e051bbe6d9cc91663b7ff04aadd0c33aea222de7
Hongyang Peng <phy654879998@foxmail.com>
2025-11-25 11:56:27 +0800

    chore:add release scripts (#600)

---
+++ scripts/gererate_newest_release_note.py
@@ -0,0 +1,32 @@
+action_map = {
+    "feat": "New features",
+    "fix": "Bug fixes",
+    "perf": "Performance improvements",
+    "docs": "Documentation",
+    "refactor": "Refactorings",
+    "style": "Style",
+    "test": "Tests",
+    "chore": "Maintenance",
+    "ci": "CI",
+    "revert": "Reverted",
+    "breaking change": "Breaking Changes"
+}
+
+
+with open("CHANGELOG.md", "r") as f:
+    for i, line in enumerate(f):
+        line = line.strip("\n")
+        if line == "":
+            break
+        if line.startswith("## "):
+            continue
+        if line.startswith("- "):
+            action_key = line[2:].strip().strip("*")
+            action_desc = action_map.get(action_key, action_key)
+            if i > 2:
+                print()
+            print(f"## {line[2:].replace(action_key, action_desc)}")
+        elif line.startswith("    "):
+            print(line[4:])
+        else:
+            raise ValueError(f"Unrecognized line format: {line}")
